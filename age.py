0a3e6628b5324eee96d065a9c496a075c2090d81
Nicole <sandiath94@gmail.com>
2022-03-03 21:49:31 +0800

    age, weather

---
+++ age.py
@@ -0,0 +1,8 @@
+age = -1
+if age < 0:
+    print('Your age is wrong')
+elif age < 18:
+    print('You are a child')
+else:
+    print('You are adult')
+print('bye')
---
+++ weather.py
@@ -0,0 +1,7 @@
+weather = 'rain'
+if weather == 'rain':
+    print('Not go to discussion club')
+    print('Ma Thiri is happy')
+else:
+    print('Go to discussion club')
+    print('Ma Thiri is sad')
